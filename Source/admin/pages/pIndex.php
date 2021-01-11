f7458a2bbeac4bf9ccd18e61a385b97e7e5dac66
GideoN <31788167+GideoNguyen@users.noreply.github.com>
2021-01-11 16:22:48 +0700

    Delete pIndex.php

--- Source/admin/pages/pIndex.php
+++
@@ -1,2 +0,0 @@
-<h1 class="col-12 text-danger text-center">Chào mừng đến với hệ thống quản trị của website MOJI SHOP</h1>
-<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
